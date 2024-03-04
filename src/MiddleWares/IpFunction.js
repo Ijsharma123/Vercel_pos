1cfb31800f7876763fddf135870702db6dfdfcae
Ijsharma123 <inderjeetsharma723@gmail.com>
2024-03-04 15:44:03 +0530

    ip new Function

--- src/MiddleWares/IpFunction.js
+++ src/MiddleWares/IpFunction.js
@@ -1,22 +1,47 @@
 const dns = require('dns')
 
+// exports.checkIpAccess = async (req, res) => {
+//     const DomainName = req.hostname
+//     console.log("DomainName is: ", DomainName)
+//     const clientIp = req.ip
+//     console.log("ClientIp is: ", clientIp)
+//     const decryptDomainIp = dns.lookup(DomainName, async function (err, decryptIp) {
+//         if (err) {
+//             console.error("Something Went Wrong:", err)
+//             res.globalResponse({ success: false, message: err })
+//         }
+//         console.log("DecryptIp is:", decryptIp)
+//         if (decryptIp == "54.183.18.207" || decryptIp == "54.183.142.227" || decryptIp == "3.24.122.159" || decryptIp == "54.79.20.55" || decryptIp == "52.62.134.160" || decryptIp == "::1" || decryptIp == "76.76.21.142") {
+//             console.log("You have Valid Ip So you Can Access")
+//             return res.globalResponse(true, '', "You have Valid Ip So you Can Access" )
+//         } else {
+//             console.log("Forbidden")
+//             return res.globalResponse(false, '', "Forbidden" )
+//         }
+//     })
+// }
+
+
 exports.checkIpAccess = async (req, res) => {
     const DomainName = req.hostname
     console.log("DomainName is: ", DomainName)
     const clientIp = req.ip
     console.log("ClientIp is: ", clientIp)
-    const decryptDomainIp = dns.lookup(DomainName, async function (err, decryptIp) {
-        if (err) {
-            console.error("Something Went Wrong:", err)
-            res.globalResponse({ success: false, message: err })
-        }
-        console.log("DecryptIp is:", decryptIp)
-        if (decryptIp == "54.183.18.207" || decryptIp == "54.183.142.227" || decryptIp == "3.24.122.159" || decryptIp == "54.79.20.55" || decryptIp == "52.62.134.160" || decryptIp == "::1" || decryptIp == "76.76.21.142") {
-            console.log("You have Valid Ip So you Can Access")
-            return res.globalResponse(true, '', "You have Valid Ip So you Can Access" )
-        } else {
-            console.log("Forbidden")
-            return res.globalResponse(false, '', "Forbidden" )
-        }
-    })
+    const socketIp = req.socket.remoteAddress
+    console.log("socketIp is: ", socketIp)
+            res.status(200).web2Response({ success: true, message:clientIp })
+    // const decryptDomainIp = dns.lookup(DomainName, async function (err, decryptIp) {
+    //     if (err) {
+    //         console.error("Something Went Wrong:", err)
+    //         res.globalResponse({ success: false, message: err })
+    //     }
+    //     console.log("DecryptIp is:", decryptIp)
+    //     if (decryptIp == "54.183.18.207" || decryptIp == "54.183.142.227" || decryptIp == "3.24.122.159" || decryptIp == "54.79.20.55" || decryptIp == "52.62.134.160" || decryptIp == "::1" || decryptIp == "15.207.23.135") {
+    //         console.log("You have Valid Ip So you Can Access")
+    //         res.status(200).web2Response({ success: true, message: "You have Valid Ip So you Can Access" })
+    //     } else {
+    //         console.log("Forbidden")
+    //         res.status(200).web2Response({ success: false, message: "Forbidden" })
+    //     }
+    // })
 }
